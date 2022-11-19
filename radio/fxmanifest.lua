22354f619b710e8f45ae7f359f47f4f7b3d6740a
Agent Kwabbel <50133696+Agent-Kwabbel@users.noreply.github.com>
2022-11-19 21:20:43 +0100

    Update fxmanifest.lua

--- radio/fxmanifest.lua
+++ radio/fxmanifest.lua
@@ -6,7 +6,7 @@ description 'This resource allows you to integrate your own radios in place of t
 version '2.0.0'
 
 -- Example custom radios
-supersede_radio 'RADIO_07_DANCE_01' { url = 'https://www.mboxdrive.com/Y2Mate.is%20-%20Boven%20Gemiddeld%20-%20Oud%20Zuid-S9lQfuBJ1ag-160k-1654746466612.mp3', volume = 0.2, name = 'Rozendaal FM' }
+supersede_radio 'RADIO_07_DANCE_01' { url = 'https://www.mboxdrive.com/Y2Mate.is%20-%20Boven%20Gemiddeld%20-%20Oud%20Zuid-S9lQfuBJ1ag-160k-1654746466612.mp3', volume = 0.2, name = 'Kakkers FM' }
 
 files {
 	'index.html'
