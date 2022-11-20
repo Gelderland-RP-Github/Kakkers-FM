3e080e6ef7c2f1b871bf93de5873f31da81eddb7
GelderlandRP <116599809+Gelderland-RP@users.noreply.github.com>
2022-11-20 16:39:23 +0100

    Update fxmanifest.lua

--- radio/fxmanifest.lua
+++ radio/fxmanifest.lua
@@ -7,6 +7,7 @@ version '2.0.0'
 
 -- Example custom radios
 supersede_radio 'RADIO_07_DANCE_01' { url = 'https://www.mboxdrive.com/Y2Mate.is%20-%20Boven%20Gemiddeld%20-%20Oud%20Zuid-S9lQfuBJ1ag-160k-1654746466612.mp3', volume = 0.2, name = 'Kakkers FM' }
+supersede_radio 'RADIO_02_POP' { url = 'https://www.mboxdrive.com/mix_48m55s%20(audio-joiner.com).mp3', volume = 0.2, name = 'Nightcore FM' }
 
 files {
 	'index.html'
